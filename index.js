c673dc990b498db3ddaf7c347ca65ed40184b91d
anavitbel <60670382+anavitbel@users.noreply.github.com>
2021-05-08 16:45:25 +0300

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const s = require('http').Server((req, res) => {
     let header = req.headers["x-test"];
 
     let result = new Object();
-    result.message = 'anavitbel';
+    result.message = 'diogenis';
     result["x-result"] = header;
     let body = "";
     
